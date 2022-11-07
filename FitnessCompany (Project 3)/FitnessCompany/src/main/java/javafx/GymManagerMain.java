3d3584381d9fb77f200940910f613d19219eece4
Raphael <michaelsliu3@gmail.com>
2022-11-07 17:58:59 -0500

    gui design

--- FitnessCompany (Project 3)/FitnessCompany/src/main/java/javafx/GymManagerMain.java
+++ FitnessCompany (Project 3)/FitnessCompany/src/main/java/javafx/GymManagerMain.java
@@ -5,27 +5,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class GymManagerMain extends Application {
 
-    private GymManagerController controller;
-
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GymManagerMain.class.getResource("GymManagerView.fxml"));
-        Parent root = fxmlLoader.load();
-        controller = fxmlLoader.getController();
 
+        Parent root = fxmlLoader.load();
         Scene scene = new Scene(root, 600, 600);
+
+        GymManagerController controller = fxmlLoader.getController();
+
+        controller.setConsole((TextArea) scene.lookup("#console"));
+        scene.getStylesheets().add(GymManagerMain.class.getResource("styles.css").toExternalForm());
+
+        stage.setResizable(false);
         stage.setTitle("Project 3 - Gym Manager");
 
         stage.setScene(scene);
-
-        controller.setConsole((TextArea) scene.lookup("#console"));
-
         stage.show();
     }
 
